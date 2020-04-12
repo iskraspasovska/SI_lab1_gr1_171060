7e2fca7f608ecf15e3dd31bcd907bd89b724d894
Iskra Spasovska <iskraspasovska99@gmail.com>
2020-04-12 17:20:02 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class Student {
 	String index;
 	String firstName;
@@ -5,14 +7,60 @@ class Student {
 	ArrayList<Integer> labPoints;
 
 	//TODO constructor
-
+	public Student(String index, String firstName, String lastName, ArrayList<Integer> labPoints) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.labPoints = labPoints;
+	}
 	//TODO seters & getters
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public ArrayList<Integer> getLabPoints() {
+		return labPoints;
+	}
+
+	public void setLabPoints(ArrayList<Integer> labPoints) {
+		this.labPoints = labPoints;
+	}
+
+
 
 	public double getAverage() {
 		//TODO
+		double average = 0;
+		for (int i = 0; i<this.labPoints.size(); i++){
+			average += this.labPoints.get(i);
+		}
+		return average/this.labPoints.size();
 	}
 
 	public boolean hasSignature() {
 		//TODO
+		double average = getAverage();
+		return average >= 8;
 	}
 }
